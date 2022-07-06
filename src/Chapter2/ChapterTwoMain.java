571118ef28cc10170f5d08c7c38c225a35bbf06a
MartinV <m.volkwyn@cimsolutions.nl>
2022-07-06 21:56:48 +0200

    update summary

---
+++ src/Chapter2/ChapterTwoMain.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+public class ChapterTwoMain {
+    public static void main(String[] args) {
+    }
+
+    void negation(){
+        double dValue = 2.2;
+        System.out.println(dValue);
+        System.out.println(-dValue);
+        System.out.println(-dValue);
+        System.out.println(-(-dValue));
+    }
+}
